1eb35406faab56c9f772817f8494d0104a671248
Angelino Eurico <angelinoeurico574@gmail.com>
2025-11-28 09:32:25 +0100

    test responsive

---
+++ script.js
@@ -0,0 +1,5 @@
+let addMenu = document.querySelector('.add-menu')
+addMenu.addEventListener('click' , ()=>{
+    let  navMenu  = document.querySelector('.nav-menu')
+    navMenu.classList.toggle('nav-menu-activo')
+})
